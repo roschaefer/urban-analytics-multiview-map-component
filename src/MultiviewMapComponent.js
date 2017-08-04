f85d2b101120949fa635eee81fb9fd6ffae46e5e
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-08-04 12:45:03 +0200

    show feature id in map component

--- src/MultiviewMapComponent.js
+++ src/MultiviewMapComponent.js
@@ -1,9 +1,29 @@
 var React = require('react');
 
-var MultiviewMapComponent = React.createClass({
-	render () {
-		return <div>Multiview Map Component</div>;
-	}
-});
+class MultiviewMapComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      featureId: '',
+      context: props.context
+    };
+    // Subscribe to featureId events.
+    this.state.context.subscribe(this, this.onFeatureId);
+  }
+  onFeatureId(featureId, that) {
+    // Update the state value for featureId.
+    that.setState({ featureId: featureId });
+  }
+  render() {
+    return (
+      <div className='alert alert-success' role='alert'>
+      <h3>Hello, from React!</h3>
+      <span>
+      { this.state.featureId }
+      </span>
+      </div>
+    );
+  }
+}
 
 export default MultiviewMapComponent;
