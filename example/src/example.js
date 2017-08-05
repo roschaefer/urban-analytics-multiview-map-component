452d62de82b43f5b823acb3f87f8119a842123a1
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-08-05 14:03:56 +0200

    fix example

--- example/src/example.js
+++ example/src/example.js
@@ -12,4 +12,25 @@ var App = React.createClass({
 	}
 });
 
-ReactDOM.render(<App />, document.getElementById('app'));
+var PublisherSubscriberManager = {
+  subscribers: [],
+  subscribe: function(parent, callback) {
+    this.subscribers.push({ parent: parent, callback: callback });
+  },
+  featureId: function(name) {
+    // Notify subscribers of event.
+    this.subscribers.forEach(function(subscriber) {
+      subscriber.callback(name, subscriber.parent);
+    });
+  }
+};
+
+ReactDOM.render(React.createElement(MultiviewMapComponent, { context: PublisherSubscriberManager }), document.getElementById('map-component'));
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  setTimeout(function() {
+    PublisherSubscriberManager.featureId('I am a featureId');
+  }, 0);
+  // Change the box featureId every few seconds.
+});
+
