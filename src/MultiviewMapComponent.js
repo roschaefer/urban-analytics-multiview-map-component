18be0e8383ad7ce6bee8f96807684f8445bc9821
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-08-14 19:26:19 +0200

    render polygons manually, to update map when geosjon url changes

--- src/MultiviewMapComponent.js
+++ src/MultiviewMapComponent.js
@@ -1,5 +1,5 @@
 import React, { Component }  from 'react'
-import { Map, Marker, Popup, TileLayer, Polygon, GeoJSON } from 'react-leaflet'
+import { Map, Marker, Popup, TileLayer, Polygon } from 'react-leaflet'
 import { DebugViewComponent } from './DebugViewComponent.js' 
 
 class MultiviewMapComponent extends Component {
@@ -42,11 +42,38 @@ class MultiviewMapComponent extends Component {
 		this.state.context.featureId = featureId;
 		this.state.context.geojsonUrl = geojsonUrl;
 	}
-	hasGeometry(){
-		return this.state.geojson != null;
+	reverseLongLat(coordinates, levels){
+		return coordinates.map((subcoordinates) =>{
+			if (levels > 1){
+				return this.reverseLongLat(subcoordinates, levels -1);
+			} else {
+				return subcoordinates.reverse();
+			}
+
+		})	
+	}
+	positionsArray(){
+		let result = this.state.geojson ? this.state.geojson.features: [];
+		result = result.map((feature) => {
+			console.log(feature);
+			let coordinates = feature.geometry.coordinates;
+			switch(feature.geometry.type){
+				case 'Polygon':
+					coordinates = this.reverseLongLat(coordinates, 2);
+					break;
+				case 'MultiPolygon':
+					coordinates = this.reverseLongLat(coordinates, 3);
+					break;
+				default:
+					console.log(feature);
+			}
+			return coordinates;
+		});
+		return result;
 	}
 	render() {
 		const position = [this.state.lat, this.state.lng];
+		console.log('render all');
 		return (
 			<div>
 			<Map center={position} zoom={this.state.zoom}>
@@ -54,9 +81,15 @@ class MultiviewMapComponent extends Component {
 			attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 			url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
 			/>
-			{ this.hasGeometry() &&
-				<GeoJSON data={this.state.geojson}>
-				</GeoJSON>
+			{ this.positionsArray().map((positions, i)=> {
+				return (
+					<Polygon
+					key={i}
+					positions={positions}
+					color='red'>
+					</Polygon >
+				)
+			})
 			}
 			<Marker position={position}>
 			<Popup>
@@ -65,11 +98,11 @@ class MultiviewMapComponent extends Component {
 			</Marker>
 			</Map>
 			{ this.state.featureId && this.state.geojsonUrl &&
-			<DebugViewComponent
+				<DebugViewComponent
 				featureId={this.state.featureId}
 				geojsonUrl={this.state.geojsonUrl}
 				handleSubmit={(featureId, geojsonUrl) => this.handleSubmit(featureId, geojsonUrl)}>
-			</DebugViewComponent>
+				</DebugViewComponent>
 			}
 			</div>
 		);
