c27da0f8d2c3114dac31810a52a02dbd32c8f1a1
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-08-24 15:54:59 +0200

    following: https://medium.com/@Jukejc/setting-up-karma-to-work-with-enzyme-mocha-and-webpack-in-2017-1ab0c2e9ef00

---
+++ karma.conf.js
@@ -0,0 +1,49 @@
+const path = require('path');
+module.exports = function(config) {
+    config.set({
+        browsers: ['PhantomJS'],
+        frameworks: ['mocha'],
+        files: [
+            'node_modules/babel-polyfill/dist/polyfill.js',
+            'test/**/*.js'
+        ],
+        preprocessors: {
+            'src/public/AppEntry.js': ['webpack', 'sourcemap'],
+            'test/**/*.js': ['webpack', 'sourcemap']
+        },
+        webpack: {
+            devtool: 'inline-source-map',
+            module: {
+                rules: [
+                    { test: /\.tsx?$/, loader: "awesome-typescript-loader", exclude: /node_modules/},
+                    { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', exclude: /node_modules/,
+                        query:
+                        {
+                            presets:['es2015', 'react'],
+                            plugins: ['transform-class-properties'],
+                            env: {
+                                development: {
+                                    presets: ['react-hmre'],
+                                },
+                            },
+                        },
+                    }],
+            },
+            externals: {
+                'react/addons': true,
+                'react/lib/ExecutionEnvironment': true,
+                'react/lib/ReactContext': true
+            }
+        },
+        webpackServer: {
+            noInfo: true
+        },
+        reporters: ['nyan'],
+
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        singleRun: false,
+    });
+};
