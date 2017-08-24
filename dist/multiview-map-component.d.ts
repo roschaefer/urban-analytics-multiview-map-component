d6f0d91c6465e4217ec8f5509181a90ee8e118f5
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-08-24 02:03:07 +0200

    add typescript declaration file

---
+++ dist/multiview-map-component.d.ts
@@ -0,0 +1,37 @@
+/// <reference types="react" />
+import * as React from "react";
+export interface DebugViewProps {
+    featureId: number;
+    geojsonUrl: string;
+    handleSubmit: (featureId: number, geojsonUrl: string) => void;
+}
+export declare class DebugView extends React.Component<DebugViewProps, DebugViewProps> {
+    constructor(props: DebugViewProps);
+    handleInputChange(event: any): void;
+    handleSubmit(event: any): void;
+    render(): JSX.Element;
+}
+export interface MultiviewMapProps {
+    context: any;
+}
+export interface MultiviewMapState {
+    context: any;
+    geojsonUrl: string;
+    geojson: any;
+    color: string;
+    featureId: number;
+    lat: number;
+    lng: number;
+    zoom: number;
+}
+export declare class MultiviewMap extends React.Component<MultiviewMapProps, MultiviewMapState> {
+    constructor(props: MultiviewMapProps);
+    componentDidMount(): void;
+    onChange(context: any, that: MultiviewMap): void;
+    handleSubmit(featureId: number, geojsonUrl: string): void;
+    reverseLongLat(coordinates: any, levels: number): any;
+    features(): any;
+    polygonColor(feature: any): "red" | "blue";
+    render(): JSX.Element;
+}
+export default MultiviewMap;
