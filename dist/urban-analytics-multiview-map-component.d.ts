7663801fdf7a14442e9a92c4f647f8a05bea6b6a
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-08-28 19:34:36 +0200

    update typescript declaration file

--- dist/urban-analytics-multiview-map-component.d.ts
+++ dist/urban-analytics-multiview-map-component.d.ts
@@ -1,30 +1,46 @@
 /// <reference types="react" />
 import * as React from "react";
+import * as Leaflet from "leaflet";
+export declare class DebugView extends React.Component<Props, State> {
+    constructor(props: Props);
+    componentWillReceiveProps(props: Props): void;
+    handleInputChange(event: any): void;
+    handleSubmit(event: React.FormEvent<HTMLFormElement>): void;
+    render(): JSX.Element;
+}
+export namespace DebugView {
+  export interface FormData {
+    featureId: number;
+    geojsonUrl: string;
+  }
+  export interface Props {
+    featureId: number;
+    geojsonUrl: string;
+    onSubmit: (formData: FormData) => void;
+  }
+  export interface State {
+    featureId: number;
+    geojsonUrl: string;
+    handleSubmit: (formData: FormData) => void;
+  }
+}
 export declare class MultiviewState {
     subscribers: any[];
     private _featureId;
     private _geojsonUrl;
+    private _geojson;
     constructor();
     featureId: number;
     geojsonUrl: string;
-    subscribe(parent: any, callback: () => void): void;
+    geojson: any;
+    subscribe(parent: any, callback: (multiviewState: MultiviewState, parent: any) => void): void;
     notify(): void;
 }
-export interface DebugViewProps {
-    featureId: number;
-    geojsonUrl: string;
-    handleSubmit: (featureId: number, geojsonUrl: string) => void;
-}
-export declare class DebugView extends React.Component<DebugViewProps, DebugViewProps> {
-    constructor(props: DebugViewProps);
-    handleInputChange(event: any): void;
-    handleSubmit(event: any): void;
-    render(): JSX.Element;
-}
-export interface MultiviewMapProps {
+namespace MultiviewMap {
+  export interface Props {
     context: any;
-}
-export interface MultiviewMapState {
+  }
+  export interface State {
     context: any;
     geojsonUrl: string;
     geojson: any;
@@ -33,15 +49,15 @@ export interface MultiviewMapState {
     lat: number;
     lng: number;
     zoom: number;
+  }
 }
-export declare class MultiviewMap extends React.Component<MultiviewMapProps, MultiviewMapState> {
-    constructor(props: MultiviewMapProps);
+export declare class MultiviewMap extends React.Component<Props, State> {
+    constructor(props: Props);
     componentDidMount(): void;
-    onChange(context: any, that: MultiviewMap): void;
-    handleSubmit(featureId: number, geojsonUrl: string): void;
-    reverseLongLat(coordinates: any, levels: number): any;
-    features(): any;
-    polygonColor(feature: any): "red" | "blue";
+    handleMultiviewStateChange(context: MultiviewState, that: MultiviewMap): void;
+    handleSubmit(formData: DebugView.FormData): void;
+    featureStyle(feature: any): Leaflet.PathOptions;
+    onEachFeature(feature: any, layer: any): void;
     render(): JSX.Element;
 }
 export default MultiviewMap;
