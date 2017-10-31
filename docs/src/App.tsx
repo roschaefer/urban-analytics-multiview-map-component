bb6567d750af9e0ca2ad5ce8c5be767a875e89c7
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-10-31 13:47:38 +0100

    Import DebugView as separate component in example app

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { MultiviewMap, MultiviewController, MessageLog } from 'urban-analytics-multiview-map-component';
+import { MultiviewMap, MultiviewController, MessageLog, DebugView } from 'urban-analytics-multiview-map-component';
 
 let controller = new MultiviewController();
 
@@ -8,6 +8,11 @@ let controller = new MultiviewController();
 ReactDOM.render(
   <MultiviewMap controller={controller} lat={undefined} lng={undefined} zoom={5}/>,
   document.getElementById('multiview-map-component')
+);
+
+ReactDOM.render(
+  <DebugView controller={controller}/>,
+  document.getElementById('multiview-debug-view')
 );
 
 ReactDOM.render(
